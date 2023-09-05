38be68af035238f14b23b32ce2583dffa38c9f4e
diadntjr <dev.dntjr0501@gmail.com>
2023-09-05 17:53:17 +0900

    Update color_seperate file
    - Add get average moments value
    - Add trans average to graph cordinate

--- modules/color_seperate.py
+++ modules/color_seperate.py
@@ -9,6 +9,15 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 
+def trans_cor(percent):
+  if(percent >= 50) :
+    cor = -1 * ((50+(percent-100))/50)
+  elif(percent < 50) :
+    cor = (50-percent)/50
+  else:
+    cor = None
+  return cor
+
 # Running part
 while cv2.waitKey(33) < 0 :
   ret, frame = capture.read()
@@ -28,6 +37,9 @@ while cv2.waitKey(33) < 0 :
   output = cv2.drawContours(seg_cone, contours, -1, (0,0,255), 3)
 
   # Calc Moments
+  avr_arr_cX = []
+  avr_arr_cY = []
+
   output_dst = output.copy()
   gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
   ret, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
@@ -41,7 +53,14 @@ while cv2.waitKey(33) < 0 :
     cY = int(M['m01'] / M['m00'])
 
     if(cX >= WIDTH/2-1 and cX <=WIDTH/2+1 and cY >= HEIGHT/2-1 and cY <= HEIGHT/2+1): continue
+    avr_arr_cX.append(cX)
+    avr_arr_cY.append(cY)
     cv2.circle(output_dst, (cX, cY), 3, (255, 100, 0), -1)
+
+  x_cor = trans_cor((np.mean(avr_arr_cX)/WIDTH)*100)
+  y_cor = trans_cor((np.mean(avr_arr_cY)/HEIGHT)*100)
+
+  # print(x_cor, y_cor)
 
   frame = cv2.flip(frame, 1)
   output_dst = cv2.flip(output_dst, 1)
